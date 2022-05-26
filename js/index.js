6f8c964b5eef2af85b9813887afd51f32887bb62
soutaisitai <84439560+soutaisitai@users.noreply.github.com>
2022-05-26 21:23:48 +0900

    主にスクロールによる色変更を調整

--- js/index.js
+++ js/index.js
@@ -1,24 +1,71 @@
-document.getElementById("color1")
+const color1 = document.getElementById("color1")
 window.addEventListener("scroll", function() {
 
     let scroll = window.pageYOffset;
 
-    if (scroll > 400) {
+    if (scroll > 480) {
         color1.style.backgroundColor = '#800000';
     } else {
         color1.style.backgroundColor = '#F2F2F2';
     }
 
 });
-document.getElementById("color1")
+const svg = document.querySelector(".svg")
 window.addEventListener("scroll", function() {
 
     let scroll = window.pageYOffset;
 
-    if (scroll > 400) {
-        color1.style.color = '#000000';
+    if (scroll > 480) {
+        svg.style.fill = '#ffffff';
     } else {
-        color1.style.color = '#FFFFFF';
+        svg.style.fill = '#000000';
+    }
+
+});
+const color2 = document.querySelector(".link1")
+var sheet = document.styleSheets[0];
+window.addEventListener("scroll", function() {
+    let scroll = window.pageYOffset;
+
+    if (scroll > 480) {
+        color2.style.color = '#ffffff';
+        sheet.insertRule('.link {color: #ffffff;}', 0);
+    } else {
+        color2.style.color = '#000000';
+        sheet.insertRule('.link {color: #000000;}', 0);
+    }
+
+});
+const color3 = document.querySelector(".link2")
+window.addEventListener("scroll", function() {
+    let scroll = window.pageYOffset;
+
+    if (scroll > 480) {
+        color3.style.color = '#ffffff';
+    } else {
+        color3.style.color = '#000000';
+    }
+
+});
+const color4 = document.querySelector(".link3")
+window.addEventListener("scroll", function() {
+    let scroll = window.pageYOffset;
+
+    if (scroll > 480) {
+        color4.style.color = '#ffffff';
+    } else {
+        color4.style.color = '#000000';
+    }
+
+});
+const color5 = document.querySelector(".link4")
+window.addEventListener("scroll", function() {
+    let scroll = window.pageYOffset;
+
+    if (scroll > 480) {
+        color5.style.color = '#ffffff';
+    } else {
+        color5.style.color = '#000000';
     }
 
 });
